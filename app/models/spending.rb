aca887bb1f3741ae10d94e2456ca18bb8afcf0bb
LebyakMarki <lebyak.m@gmail.com>
2023-01-19 21:29:08 +0100

    Added amount filtering

--- app/models/spending.rb
+++ app/models/spending.rb
@@ -1,15 +1,21 @@
 class Spending < ApplicationRecord
   belongs_to :user
 
-  FILTER_PARAMS = %i[description category column direction].freeze
+  FILTER_PARAMS = %i[description amount_from amount_to category column direction].freeze
 
   scope :by_category, ->(category) { where(category: category) if category.present? }
+  scope :by_amount_from, ->(amount_from) { where("spendings.amount > ?", amount_from) if amount_from.present? }
+  scope :by_amount_to, ->(amount_to) { where("spendings.amount < ?", amount_to) if amount_to.present? }
   scope :by_description, ->(query) { where('spendings.description like ?', "%#{query}%") }
+
+
 
   def self.filter(filters)
     Spending
       .by_description(filters['description'])
       .by_category(filters['category'])
+      .by_amount_from(filters['amount_from'])
+      .by_amount_to(filters['amount_to'])
       .order("#{filters['column']} #{filters['direction']}")
   end
 end
