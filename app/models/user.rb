82262598e43bbc9ab3841b38d8f9760af2691f5c
LebyakMarki <lebyak.m@gmail.com>
2023-01-21 11:44:43 +0100

    Changed User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :spending,
+  has_many :spendings,
            foreign_key: :user_id
-  has_many :share
-  has_many :table_shared_with_me,
-           through: :share,
-           source: :spending
+  has_many :shares
+  has_many :tables_shared_with_me,
+           through: :shares,
+           source: :spendings
 end
