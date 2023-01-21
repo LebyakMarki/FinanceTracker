69cbe1b4119523cd3817352f1a8fb9db6762536a
LebyakMarki <lebyak.m@gmail.com>
2023-01-21 11:49:14 +0100

    Changed User Model to add shares

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :spendings,
+  has_many :spending,
            foreign_key: :user_id
   has_many :shares
   has_many :tables_shared_with_me,
            through: :shares,
-           source: :spendings
+           source: :spending
 end
